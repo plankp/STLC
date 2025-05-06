589bae66ff9e21e48fdf1939635a5731fdea3e63
Paul Teng <plankp@outlook.com>
2025-05-06 13:40:08 -0400

    Write explicit proof terms for extracted functions

--- STLC/Ela.v
+++ STLC/Ela.v
@@ -4,15 +4,13 @@ From STLC Require Import Typ Ast Lir.
 Inductive hidden (f : tp -> Type) : Type :=
   | Ev : forall t, f t -> hidden f.
 
-Definition weaken_s (g : ctx) (s : string -> option (hidden (db g))) (n : string) (t : tp) : string -> option (hidden (db (Cons g t))).
-Proof.
-  intros n'.
-  destruct (string_dec n' n).
-  + apply (Some (Ev _ t (H _ t))).
-  + destruct (s n') as [[t' d']|].
-    apply (Some (Ev _ t' (U _ _ _ d'))).
-    apply None.
-Defined.
+Definition weaken_s (g : ctx) (s : string -> option (hidden (db g))) (n : string) (t : tp) : string -> option (hidden (db (Cons g t))) :=
+  fun n' => if string_dec n' n
+            then Some (Ev _ t (H _ t))
+            else match s n' with
+                 | Some (Ev _ t' d') => Some (Ev _ t' (U _ _ _ d'))
+                 | _ => None
+                 end.
 
 Lemma weaken_s_adds_a_new_binding:
   forall g (s : string -> option (hidden (db g))) n t, (weaken_s g s n t) n = Some (Ev _ _ (H _ t)).
@@ -58,243 +56,149 @@ Inductive elabres (T : Type) : Type :=
   | NeedAnnot : elabres T
   | WrongType : elabres T.
 
-Fixpoint elab_infer (g : ctx) (s : string -> option (hidden (db g))) (e : ast) {struct e} : elabres (hidden (tm g))
-  with elab_check (g : ctx) (s : string -> option (hidden (db g))) (e : ast) (t : tp) {struct e} : elabres (tm g t).
-Proof.
-  - (* defining elab_infer *)
-    destruct e.
-    * (* EVar *)
-      destruct (s s0) as [[t n]|].
-      apply (Ok _ (Ev _ t (Var _ _ n))).
-      apply (UndefName _ s0).
-    * (* ELam *)
-      destruct o as [ta|].
-      destruct (elab_infer _ (weaken_s _ s s0 ta) e) as [[tr body]|err| | ].
-      apply (Ok _ (Ev _ (TArr ta tr) (Lam _ _ _ body))).
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-      apply NeedAnnot.
-    * (* EApp *)
-      destruct (elab_infer _ s e1) as [[[ta tr| |c1 c2|i1 i2] tm1]|err| |].
-      + (* we inferred e1 as ta -> tr *)
-        destruct (elab_check _ s e2 ta) as [tm2|err| |].
-        apply (Ok _ (Ev _ tr (App _ _ _ tm1 tm2))).
-        apply (UndefName _ err).
-        apply NeedAnnot.
-        apply WrongType.
-      + apply WrongType.
-      + apply WrongType.
-      + apply WrongType.
-      + apply (UndefName _ err).
-      + apply NeedAnnot.
-      + apply WrongType.
-    * (* EUnit *)
-      apply (Ok _ (Ev _ TUnit (Unit _))).
-    * (* ELet *)
-      destruct (elab_infer _ s e1) as [[t1 tm1]|err| | ].
-      + (* we inferred e1 *)
-        destruct (elab_infer _ (weaken_s _ s s0 t1) e2) as [[t2 tm2]|err| |].
-        apply (Ok _ (Ev _ t2 (Let _ _ _ tm1 tm2))).
-        apply (UndefName _ err).
-        apply NeedAnnot.
-        apply WrongType.
-      + apply (UndefName _ err).
-      + apply NeedAnnot.
-      + apply WrongType.
-    * (* EPair *)
-      destruct (elab_infer _ s e1) as [[t1 tm1]|err| |].
-      destruct (elab_infer _ s e2) as [[t2 tm2]|err| |].
-      apply (Ok _ (Ev _ (TPair t1 t2) (Pair _ _ _ tm1 tm2))).
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-    * (* EFst *)
-      destruct (elab_infer _ s e) as [[[_1 _2| |t1 t2|i1 i2] tm]|err| |].
-      apply WrongType.
-      apply WrongType.
-      apply (Ok _ (Ev _ t1 (Fst _ _ _ tm))).
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-    * (* ESnd *)
-      destruct (elab_infer _ s e) as [[[_1 _2| |t1 t2|i1 i2] tm]|err| |].
-      apply WrongType.
-      apply WrongType.
-      apply (Ok _ (Ev _ t2 (Snd _ _ _ tm))).
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-    * (* EInl *)
-      apply NeedAnnot.
-    * (* EInr *)
-      apply NeedAnnot.
-    * (* ECase *)
-      destruct (elab_infer _ s e1) as [[[_1 _2| |c1 c2|i1 i2] tm1]|err| |].
-      apply WrongType.
-      apply WrongType.
-      apply WrongType.
-      destruct (elab_infer _ (weaken_s _ s s0 i1) e2) as [[tr tm2]|err| |].
-      destruct (elab_check _ (weaken_s _ s s0 i2) e3 tr) as [tm3|err| |].
-      apply (Ok _ (Ev _ tr (Case _ _ _ _ tm1 tm2 tm3))).
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-    * (* EAnn *)
-      destruct (elab_check _ s e t).
-      apply (Ok _ (Ev _ t t0)).
-      apply (UndefName _ s0).
-      apply NeedAnnot.
-      apply WrongType.
+Definition bind_elabres {A B} (e : elabres A) (f : A -> elabres B) : elabres B :=
+  match e with
+  | Ok _ x => f x
+  | UndefName _ n => UndefName _ n
+  | NeedAnnot _ => NeedAnnot _
+  | WrongType _ => WrongType _
+  end.
 
-  - (* defining elab_check *)
-    destruct e.
-    * (* EVar *)
-      destruct (s s0) as [[t0 n]|].
-      + (* variable exists *)
-        destruct (tp_eq_dec t0 t).
-        rewrite <- e. apply (Ok _ (Var _ _ n)).
-        apply WrongType.
-      + (* variable does not exist *)
-        apply (UndefName _ s0).
-    * (* ELam *)
-      destruct t as [ta tr| | c1 c2|i1 i2].
-      + (* ta -> tr *)
-        destruct o as [ta'|].
-        ++ destruct (tp_eq_dec ta' ta).
-           destruct (elab_check _ (weaken_s _ s s0 ta) e tr) as [body|err| |].
-           apply (Ok _ (Lam _ _ _  body)).
-           apply (UndefName _ err).
-           apply NeedAnnot.
-           apply WrongType.
-           apply WrongType.
-        ++ destruct (elab_check _ (weaken_s _ s s0 ta) e tr) as [body|err| |].
-           apply (Ok _ (Lam _ _ _  body)).
-           apply (UndefName _ err).
-           apply NeedAnnot.
-           apply WrongType.
-      + apply WrongType. (* a whole bunch of type-mismatch cases *)
-      + apply WrongType.
-      + apply WrongType.
-    * (* EApp *)
-      destruct (elab_infer _ s e1) as [[[ta tr| | c1 c2|i1 i2] tm1]|err| |].
-      + (* we inferreed e1 as ta -> tr *)
-        destruct (tp_eq_dec tr t).
-        ++ destruct (elab_check _ s e2 ta) as [tm2|err| |].
-           rewrite <- e. apply (Ok _ (App _ _ _ tm1 tm2)).
-           apply (UndefName _ err).
-           apply NeedAnnot.
-           apply WrongType.
-        ++ apply WrongType.
-      + apply WrongType. (* a whole bunch of type-mismatch cases *)
-      + apply WrongType.
-      + apply WrongType.
-      + apply (UndefName _ err).
-      + apply NeedAnnot.
-      + apply WrongType.
-    * (* EUnit *)
-      destruct (tp_eq_dec TUnit t).
-      rewrite <- e. apply (Ok _ (Unit _)).
-      apply WrongType.
-    * (* ELet *)
-      destruct (elab_infer _ s e1) as [[t1 tm1]|err| |].
-      + (* we inferred e1 *)
-        destruct (elab_check _ (weaken_s _ s s0 t1) e2 t) as [tm2|err| |].
-        apply (Ok _ (Let _ _ _ tm1 tm2)).
-        apply (UndefName _ err).
-        apply NeedAnnot.
-        apply WrongType.
-      + apply (UndefName _ err).
-      + apply NeedAnnot.
-      + apply WrongType.
-    * (* EPair *)
-      destruct t as [_1 _2| |t1 t2|i1 i2].
-      + apply WrongType.
-      + apply WrongType.
-      + (* t1 * t2 *)
-        destruct (elab_check _ s e1 t1) as [tm1|err| |].
-        destruct (elab_check _ s e2 t2) as [tm2|err| |].
-        apply (Ok _ (Pair _ _ _ tm1 tm2)).
-        apply (UndefName _ err).
-        apply NeedAnnot.
-        apply WrongType.
-        apply (UndefName _ err).
-        apply NeedAnnot.
-        apply WrongType.
-      + apply WrongType.
-    * (* EFst *)
-      destruct (elab_infer _ s e) as [[[_1 _2| |t1 t2|i1 i2] tm]|err| |].
-      apply WrongType.
-      apply WrongType.
-      destruct (tp_eq_dec t1 t).
-      rewrite <- e0. apply (Ok _ (Fst _ _ _ tm)).
-      apply WrongType.
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-    * (* ESnd *)
-      destruct (elab_infer _ s e) as [[[_1 _2| |t1 t2|i1 i2] tm]|err| |].
-      apply WrongType.
-      apply WrongType.
-      destruct (tp_eq_dec t2 t).
-      rewrite <- e0. apply (Ok _ (Snd _ _ _ tm)).
-      apply WrongType.
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-    * (* EInl *)
-      destruct t as [ta tr| |c1 c2|i1 i2].
-      apply WrongType.
-      apply WrongType.
-      apply WrongType.
-      destruct (elab_check _ s e i1) as [tm|err| |].
-      + apply (Ok _ (Inl _ _ _ tm)).
-      + apply (UndefName _ err).
-      + apply NeedAnnot.
-      + apply WrongType.
-    * (* EInr *)
-      destruct t as [ta tr| |c1 c2|i1 i2].
-      apply WrongType.
-      apply WrongType.
-      apply WrongType.
-      destruct (elab_check _ s e i2) as [tm|err| |].
-      + apply (Ok _ (Inr _ _ _ tm)).
-      + apply (UndefName _ err).
-      + apply NeedAnnot.
-      + apply WrongType.
-    * (* ECase *)
-      destruct (elab_infer _ s e1) as [[[_1 _2| |c1 c2|i1 i2] tm1]|err| |].
-      apply WrongType.
-      apply WrongType.
-      apply WrongType.
-      destruct (elab_check _ (weaken_s _ s s0 i1) e2 t) as [tm2|err| |].
-      destruct (elab_check _ (weaken_s _ s s0 i2) e3 t) as [tm3|err| |].
-      apply (Ok _ (Case _ _ _ _ tm1 tm2 tm3)).
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-      apply (UndefName _ err).
-      apply NeedAnnot.
-      apply WrongType.
-    * (* EAnn *)
-      destruct (tp_eq_dec t0 t).
-      rewrite <- e0. apply (elab_check _ s e t0).
-      apply WrongType.
-Defined.
+Fixpoint infer'
+  (check' : forall g, (string -> option (hidden (db g))) -> ast -> forall t, elabres (tm g t))
+  (g : ctx) (s : string -> option (hidden (db g))) (e : ast) {struct e} : elabres (hidden (tm g)) :=
+  match e with
+  | EVar n =>
+      match s n with
+      | Some (Ev _ t n) => Ok _ (Ev _ t (Var _ _ n))
+      | _ => UndefName _ n
+      end
+  | EAnn e t =>
+      bind_elabres (check' _ s e t) (fun e =>
+      Ok _ (Ev _ _ e))
+  | EUnit =>
+      Ok _ (Ev _ TUnit (Unit _))
+  | ELam n (Some ta) m =>
+      bind_elabres (infer' check' _ (weaken_s _ s n ta) m) (fun m =>
+      match m with
+      | Ev _ tr body => Ok _ (Ev _ (TArr ta tr) (Lam _ _ _ body))
+      end)
+  | EApp e1 e2 =>
+      bind_elabres (infer' check' _ s e1) (fun e1 =>
+      match e1 with
+      | Ev _ (TArr ta tr) e1 =>
+          bind_elabres (check' _ s e2 ta) (fun e2 =>
+          Ok _ (Ev _ tr (App _ _ _ e1 e2)))
+      | _ => WrongType _
+      end)
+  | EPair e1 e2 =>
+      bind_elabres (infer' check' _ s e1) (fun e1 =>
+      bind_elabres (infer' check' _ s e2) (fun e2 =>
+      match e1, e2 with
+      | Ev _ t1 e1, Ev _ t2 e2 => Ok _ (Ev _ (TPair t1 t2) (Pair _ _ _ e1 e2))
+      end))
+  | EFst e =>
+      bind_elabres (infer' check' _ s e) (fun e =>
+      match e with
+      | Ev _ (TPair t1 t2) e => Ok _ (Ev _ t1 (Fst _ _ _ e))
+      | _ => WrongType _
+      end)
+  | ESnd e =>
+      bind_elabres (infer' check' _ s e) (fun e =>
+      match e with
+      | Ev _ (TPair t1 t2) e => Ok _ (Ev _ t2 (Snd _ _ _ e))
+      | _ => WrongType _
+      end)
+  | ECase e n1 e1 n2 e2 =>
+      bind_elabres (infer' check' _ s e) (fun e =>
+      match e with
+      | Ev _ (TInj t1 t2) e =>
+          bind_elabres (infer' check' _ (weaken_s _ s n1 t1) e1) (fun e1 =>
+          match e1 with
+          | Ev _ tr e1 =>
+              bind_elabres (check' _ (weaken_s _ s n2 t2) e2 tr) (fun e2 =>
+              Ok _ (Ev _ tr (Case _ _ _ _ e e1 e2)))
+          end)
+      | _ => WrongType _
+      end)
+  | ELet n1 e1 e2 =>
+      bind_elabres (infer' check' _ s e1) (fun e =>
+      match e with
+      | Ev _ t1 e1 =>
+          bind_elabres (infer' check' _ (weaken_s _ s n1 t1) e2) (fun e2 =>
+          match e2 with
+          | Ev _ t2 e2 => Ok _ (Ev _ t2 (Let _ _ _ e1 e2))
+          end)
+      end)
+  | _ =>
+      NeedAnnot _
+  end.
+
+Fixpoint check'
+  (g : ctx) (s : string -> option (hidden (db g))) (e : ast) (t : tp) {struct e} : elabres (tm g t) :=
+  match e, t with
+  | EAnn e t', _ =>
+      match tp_eq_dec t' t with
+      | left Heq =>
+          (* due to Prop erasure, this is actually a tail call to check' *)
+          match Heq with eq_refl => check' _ s e t' end
+      | _ => WrongType _
+      end
+  | ELam n None m, TArr ta tr =>
+      bind_elabres (check' _ (weaken_s _ s n ta) m tr) (fun m =>
+      Ok _ (Lam _ _ _ m))
+  | ELam n (Some t') m, TArr ta tr =>
+      match tp_eq_dec t' ta with
+      | left Heq =>
+          bind_elabres (check' _ (weaken_s _ s n t') m tr) (fun m =>
+          Ok _ (Lam _ _ _ (match Heq with eq_refl => m end)))
+      | _ => WrongType _
+      end
+  | ELam _ _ _, _ =>
+      WrongType _
+  | EPair e1 e2, TPair t1 t2 =>
+      bind_elabres (check' g s e1 t1) (fun e1 =>
+      bind_elabres (check' g s e2 t2) (fun e2 =>
+      Ok _ (Pair _ _ _ e1 e2)))
+  | EPair _ _, _ =>
+      WrongType _
+  | EInl e, TInj t1 t2 =>
+      bind_elabres (check' g s e t1) (fun e =>
+      Ok _ (Inl _ _ _ e))
+  | EInl _, _ =>
+      WrongType _
+  | EInr e, TInj t1 t2 =>
+      bind_elabres (check' g s e t2) (fun e =>
+      Ok _ (Inr _ _ _ e))
+  | EInr _, _ =>
+      WrongType _
+  | ECase e n1 e1 n2 e2, _ =>
+      bind_elabres (infer' check' g s e) (fun e =>
+      match e with
+      | Ev _ (TInj t1 t2) e =>
+          bind_elabres (check' _ (weaken_s _ s n1 t1) e1 t) (fun e1 =>
+          bind_elabres (check' _ (weaken_s _ s n2 t2) e2 t) (fun e2 =>
+          Ok _ (Case _ _ _ _ e e1 e2)))
+      | _ => WrongType _
+      end)
+  | ELet n1 e1 e, _ =>
+      bind_elabres (infer' check' g s e1) (fun e1 =>
+      match e1 with
+      | Ev _ t1 e1 =>
+          bind_elabres (check' _ (weaken_s _ s n1 t1) e t) (fun e =>
+          Ok _ (Let _ _ _ e1 e))
+      end)
+  | _, _ =>
+      bind_elabres (infer' check' g s e) (fun e =>
+      match e with
+      | Ev _ t' e =>
+          match tp_eq_dec t' t with
+          | left Heq => Ok _ (match Heq with eq_refl => e end)
+          | _ => WrongType _
+          end
+      end)
+  end.
+
+Definition elab_infer := infer' check'.
+Definition elab_check := check'.
 
